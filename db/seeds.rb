b87c61e4b3e9f921443973cf80176372d2d2aeec
oclivar <oclivar2000@gmail.com>
2024-09-12 23:41:11 -0300

    Agragando estilos a las vista clientes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'faker'
+
 100.times do
   Movie.create!([
     { name: Faker::Movie.title }
@@ -17,3 +19,28 @@ end
     { name: Faker::Name.name, age: Faker::Number.between(from: 18, to: 100) }
   ])
 end
+
+
+# db/seeds.rb
+
+
+
+# # Crea 20 películas ficticias
+# 20.times do
+#   Movie.create!(
+#     title: Faker::Movie.title,
+#     director: Faker::Name.name,
+#     synopsis: Faker::Lorem.paragraph(sentence_count: 3)
+#   )
+# end
+
+# # Crea 20 clientes ficticios
+# 20.times do
+#   Client.create!(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.email,
+#     address: Faker::Address.full_address
+#   )
+# end
+
+# puts "20 películas y 20 clientes han sido creados exitosamente."
